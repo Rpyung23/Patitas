829afffbee81910ebecd33267ee0a5f90ebc6b5d
Rpyung23 <guaman1579@gmail.com>
2021-11-26 23:32:31 -0600

    Initial commit

---
+++ app/src/main/java/com/franciscorivera/patitas/view/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.franciscorivera.patitas.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
